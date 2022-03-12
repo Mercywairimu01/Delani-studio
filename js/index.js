6ad30fcdb4a652117778040e1ea17b1215fb12d4
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-03-13 01:30:13 +0300

    make the portfolio section responsive by adding a mouseover effect to show .overlay

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// business logic
 $(document).ready(function(){
     $(".designers").click(function(){
         $(".design").toggle();
@@ -25,3 +26,56 @@ $(document).ready(function(){
     })
 
 });
+
+// portfolio section
+$(document).ready(function(){
+    $(".work4").mouseover(function(){
+      $("#over").show();
+    }).mouseout(function(){
+      $("#over").hide();
+    });
+  
+ 
+    $(".work3").mouseover(function(){
+      $("#over2").show();
+    }).mouseout(function(){
+      $("#over2").hide();
+    });
+  
+    $(".work2").mouseover(function(){
+      $("#over3").show();
+    }).mouseout(function(){
+      $("#over3").hide();
+    });
+
+    $(".work1").mouseover(function(){
+      $("#over4").show();
+    }).mouseout(function(){
+      $("#over4").hide();
+    });
+ 
+    $(".work5").mouseover(function(){
+      $("#over5").show();
+    }).mouseout(function(){
+      $("#over5").hide();
+    });
+  
+    $(".work6").mouseover(function(){
+      $("#over6").show();
+    }).mouseout(function(){
+      $("#over6").hide();
+    });
+
+    $(".work7").mouseover(function(){
+      $("#over7").show();
+    }).mouseout(function(){
+      $("#over7").hide();
+    });
+  
+    $(".work8").mouseover(function(){
+      $("#over8").show();
+    }).mouseout(function(){
+      $("#over8").hide();
+    });
+  });
+
